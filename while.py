e2b033c3eca5bff22968170fa7bca778debd4f95
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-09-20 19:05:21 +0100

    while.py

---
+++ while.py
@@ -0,0 +1,11 @@
+import random
+
+# while loop when you don't know how many times you gonna loop
+
+rand_num = random.randrange(1,51)
+
+i = 1
+
+while (i != rand_num):
+    i += 1
+print("the random value is : ", rand_num)
