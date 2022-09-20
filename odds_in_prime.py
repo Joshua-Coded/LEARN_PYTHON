8aec4de02239f62714fd7ae88d750899da8e4e6b
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-09-20 14:06:26 +0100

    odds_in_prime.py

---
+++ odds_in_prime.py
@@ -0,0 +1,8 @@
+#use for loop through the list from 1 to 21
+for i in range(1, 21):
+# use modulus to check that the result is not equal to 0
+    if ((i % 2) != 0):
+       
+
+#print the odds
+       print("i = ", i)
