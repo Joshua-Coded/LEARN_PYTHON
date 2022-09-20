d94ce131c0411ae66de183951f435bd7a9565a5a
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-09-20 14:06:08 +0100

    miles2kil.py

---
+++ miles2kil.py
@@ -0,0 +1,13 @@
+#ask the users for miles 
+miles = input("Enter miles: ")
+
+# convert string to int
+miles = int(miles)
+
+
+
+kilometers = miles * 1.60934
+print("{} miles equals {} kilometers".format(miles, kilometers ))
+
+
+
